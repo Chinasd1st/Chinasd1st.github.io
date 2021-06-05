82f2fdb9f948f68b0f3879e29e36a1e72c0cae31
Chinasd1st <1297537384@qq.com>
2021-06-05 12:53:12 +0800

    6/5 新文章

--- assets/search.js
+++ assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Test",'url':"/2021/06/04/test.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Test",'url':"/2021/06/04/test.html"},{'title':"创建个人blog心得",'url':"/2021/06/05/%E5%88%9B%E5%BB%BA%E4%B8%AA%E4%BA%BAblog%E5%BF%83%E5%BE%97.html"}]};
