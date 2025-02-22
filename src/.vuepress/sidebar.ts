bdbc8ce49bf5e1829b1ee8b419fb1910fc1c3f65
Chinasd1st <68810179+Chinasd1st@users.noreply.github.com>
2025-02-22 17:02:52 +0800

    modified:   package-lock.json 	modified:   package.json 	modified:   src/.vuepress/config.ts 	modified:   src/.vuepress/sidebar.ts 	modified:   src/.vuepress/theme.ts 	new file:   src/post/GirlsBands.md

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -5,8 +5,18 @@ export default sidebar({
     "",
     {
       text: "文章存档",
+      collapsible: true,
+      expanded: true,  
       icon: "book",
       prefix: "old/",
+      children: "structure",
+    },
+    {
+      text: "博文",
+      collapsible: true,
+      expanded: true,  
+      icon: "book",
+      prefix: "post/",
       children: "structure",
     },
     "intro",
