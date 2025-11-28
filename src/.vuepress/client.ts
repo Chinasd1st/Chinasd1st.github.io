d1cb08ca465644e8870dd6042ca0a4ee4510a2db
Chinasd1st <1297537384@qq.com>
2025-11-28 22:10:23 +0800

    feat: 添加 BusuanziStats 组件以显示页面访问量信息

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -1,5 +1,6 @@
 import { defineClientConfig } from "vuepress/client";
 import Blog from "./layouts/Blog.vue";
+import BusuanziStats from "./components/BusuanziStats.vue";
 
 export default defineClientConfig({
   //...
@@ -8,4 +9,12 @@ export default defineClientConfig({
     // ...
     Blog,
   },
+  enhance({ app }) {
+    // 注册组件
+    app.component("BusuanziStats", BusuanziStats);
+  },
+
+  // ⚠️ 注意：删除原来的 layouts 配置块
+  // 改用 rootComponents，这样组件会挂载在页面根部，不会破坏原有 Layout
+  rootComponents: [BusuanziStats],
 });
