833446458b50b5c0998aa33725ace52fb9681281
Chinasd1st <68810179+Chinasd1st@users.noreply.github.com>
2025-12-21 06:20:04 +0000

    Deploying to gh-pages from @ Chinasd1st/Chinasd1st.github.io@e34c6bc9727ffcd0e874cc658fd4414be472f7a4 🚀

---
+++ assets/VideosCompilation.html-DT_FPqiv.js
@@ -0,0 +1,36 @@
+import{a as e,c as s,b as t,g as o,f as n,o as a}from"./app-DTxLo1VT.js";const c={};function p(d,i){return a(),s("div",null,[i[0]||(i[0]=t("h1",{id:"抽象视频合辑",tabindex:"-1"},[t("a",{class:"header-anchor",href:"#抽象视频合辑"},[t("span",null,"抽象视频合辑")])],-1)),i[1]||(i[1]=t("p",null,[t("s",null,"为防止一些视频灭失，现存档于小奶奶网站。"),n("（20251130更新）由于Github本身并不是合适的CDN，外加上传大量视频会导致部署时间大幅增长，现移除这些视频。")],-1)),o(`
+<VidStack
+    src="https://chinasd1st.github.io/videos/compilation/1.mp4"
+/>
+<VidStack
+    src="https://chinasd1st.github.io/videos/compilation/2.mp4"
+/>
+<VidStack
+    src="https://chinasd1st.github.io/videos/compilation/3.mp4"
+/>
+<VidStack
+    src="https://chinasd1st.github.io/videos/compilation/4.mp4"
+/>
+<VidStack
+    src="https://chinasd1st.github.io/videos/compilation/5.mp4"
+/>
+<VidStack
+    src="https://chinasd1st.github.io/videos/compilation/6.mp4"
+/>
+<VidStack
+    src="https://chinasd1st.github.io/videos/compilation/7.mp4"
+/>
+<VidStack
+    src="https://chinasd1st.github.io/videos/compilation/8.mp4"
+/>
+<VidStack
+    src="https://chinasd1st.github.io/videos/compilation/9.mp4"
+/>
+<VidStack
+    src="https://chinasd1st.github.io/videos/compilation/10.mp4"
+/>
+<VidStack
+    src="https://chinasd1st.github.io/videos/compilation/11.mp4"
+/>
+
+`)])}const r=e(c,[["render",p]]),l=JSON.parse('{"path":"/post/2025/08/VideosCompilation.html","title":"抽象视频合辑","lang":"zh-CN","frontmatter":{"date":"2025-08-13T00:00:00.000Z","category":"存档","tag":["视频"],"icon":"video","description":"抽象视频合辑 （20251130更新）由于Github本身并不是合适的CDN，外加上传大量视频会导致部署时间大幅增长，现移除这些视频。","head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"Article\\",\\"headline\\":\\"抽象视频合辑\\",\\"image\\":[\\"\\"],\\"datePublished\\":\\"2025-08-13T00:00:00.000Z\\",\\"dateModified\\":\\"2025-11-30T14:06:14.000Z\\",\\"author\\":[{\\"@type\\":\\"Person\\",\\"name\\":\\"Silentnrtx\\",\\"url\\":\\"https://chinasd1st.github.io\\"}]}"],["meta",{"property":"og:url","content":"https://chinasd1st.github.io/post/2025/08/VideosCompilation.html"}],["meta",{"property":"og:site_name","content":"小奶奶博客"}],["meta",{"property":"og:title","content":"抽象视频合辑"}],["meta",{"property":"og:description","content":"抽象视频合辑 （20251130更新）由于Github本身并不是合适的CDN，外加上传大量视频会导致部署时间大幅增长，现移除这些视频。"}],["meta",{"property":"og:type","content":"article"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2025-11-30T14:06:14.000Z"}],["meta",{"property":"article:tag","content":"视频"}],["meta",{"property":"article:published_time","content":"2025-08-13T00:00:00.000Z"}],["meta",{"property":"article:modified_time","content":"2025-11-30T14:06:14.000Z"}]]},"git":{"createdTime":1755060054000,"updatedTime":1764511574000,"contributors":[{"name":"Chinasd1st","username":"Chinasd1st","email":"1297537384@qq.com","commits":5,"url":"https://github.com/Chinasd1st"}],"changelog":[{"hash":"6630716e6f7e7a163529379a2cbd3c12347a1008","time":1764511574000,"email":"1297537384@qq.com","author":"Chinasd1st","message":"feat: 移除视频文件以优化存储，更新相关文档说明"},{"hash":"50381ca64b1e8390492e533e61aff2d0e6c843aa","time":1762867201000,"email":"1297537384@qq.com","author":"Chinasd1st","message":"optimized the directory of articles"},{"hash":"f6c0befedd78da46c1a883285ed138134d15ca0c","time":1755335053000,"email":"1297537384@qq.com","author":"Chinasd1st","message":"Add icons to the navigation bar and related content, and update the metadata of multiple articles to include icons."},{"hash":"766e212df6a1ab89049c8f39f07c1d01e1b9da39","time":1755078860000,"email":"1297537384@qq.com","author":"Chinasd1st","message":"new file:   src/.vuepress/client.ts \\tmodified:   src/.vuepress/config.ts \\tnew file:   src/.vuepress/layouts/Blog.vue \\tmodified:   src/.vuepress/styles/index.scss \\tmodified:   src/old/Playground.md     modified:   src/old/RyoukiArticle.md \\tmodified:   src/old/news.md \\tmodified:   src/post/VideosCompilation.md"},{"hash":"83e4467e8638e634ebb96bb8daece21880a9935f","time":1755060054000,"email":"1297537384@qq.com","author":"Chinasd1st","message":"new file:   src/.vuepress/public/videos/compilation/1.mp4 \\tnew file:   src/.vuepress/public/videos/compilation/10.mp4 \\tnew file:   src/.vuepress/public/videos/compilation/11.mp4 \\tnew file:   src/.vuepress/public/videos/compilation/2.mp4 \\tnew file:   src/.vuepress/public/videos/compilation/3.mp4 \\tnew file:   src/.vuepress/public/videos/compilation/4.mp4 \\tnew file:   src/.vuepress/public/videos/compilation/5.mp4 \\tnew file:   src/.vuepress/public/videos/compilation/6.mp4 \\tnew file:   src/.vuepress/public/videos/compilation/7.mp4 \\tnew file:   src/.vuepress/public/videos/compilation/8.mp4 \\tnew file:   src/.vuepress/public/videos/compilation/9.mp4 \\tnew file:   src/post/VideosCompilation.md"}]},"readingTime":{"minutes":0.44,"words":132},"filePathRelative":"post/2025/08/VideosCompilation.md","excerpt":"\\n<p><s>为防止一些视频灭失，现存档于小奶奶网站。</s>（20251130更新）由于Github本身并不是合适的CDN，外加上传大量视频会导致部署时间大幅增长，现移除这些视频。</p>\\n<!--\\n<VidStack\\n    src=\\"https://chinasd1st.github.io/videos/compilation/1.mp4\\"\\n/>\\n<VidStack\\n    src=\\"https://chinasd1st.github.io/videos/compilation/2.mp4\\"\\n/>\\n<VidStack\\n    src=\\"https://chinasd1st.github.io/videos/compilation/3.mp4\\"\\n/>\\n<VidStack\\n    src=\\"https://chinasd1st.github.io/videos/compilation/4.mp4\\"\\n/>\\n<VidStack\\n    src=\\"https://chinasd1st.github.io/videos/compilation/5.mp4\\"\\n/>\\n<VidStack\\n    src=\\"https://chinasd1st.github.io/videos/compilation/6.mp4\\"\\n/>\\n<VidStack\\n    src=\\"https://chinasd1st.github.io/videos/compilation/7.mp4\\"\\n/>\\n<VidStack\\n    src=\\"https://chinasd1st.github.io/videos/compilation/8.mp4\\"\\n/>\\n<VidStack\\n    src=\\"https://chinasd1st.github.io/videos/compilation/9.mp4\\"\\n/>\\n<VidStack\\n    src=\\"https://chinasd1st.github.io/videos/compilation/10.mp4\\"\\n/>\\n<VidStack\\n    src=\\"https://chinasd1st.github.io/videos/compilation/11.mp4\\"\\n/>\\n\\n-->","autoDesc":true}');export{r as comp,l as data};
